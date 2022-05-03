14ff43aa7956d8d1f8b098205b904396dfc15354
klupsky <youremail@example.com>
2022-05-03 18:21:23 +0200

    Add hue and luminosity

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
+import randomColor from 'randomcolor';
 
-const randomColor = '#' + Math.floor(Math.random() * 16777216).toString(16);
+const color = '#' + Math.floor(Math.random() * 16777216).toString(16);
 
 const row = [
   `   ###############################
@@ -8,7 +9,7 @@ const row = [
    ###############################
    #####                    ######
    #####                    ######
-   #####      ${randomColor}       ######
+   #####      ${color}       ######
    #####                    ######
    #####                    ######
    ###############################
@@ -17,4 +18,19 @@ const row = [
   `,
 ];
 
-console.log(chalk.hex(randomColor).bold(row));
+console.log(chalk.hex(color).bold(row));
+console.log(
+  randomColor({
+    luminosity: 'light',
+    hue: 'blue',
+  }),
+);
+
+console.log(
+  chalk.hex(
+    randomColor({
+      luminosity: process.argv[3],
+      hue: process.argv[2],
+    }),
+  )('string'),
+);
