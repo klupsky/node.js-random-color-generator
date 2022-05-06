0887acfc47cf0c9a637fbb887a3b8413c80d1aac
klupsky <youremail@example.com>
2022-05-06 16:48:28 +0200

    Add final version

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// import chalk from npm
 import chalk from 'chalk';
+// import randomColor from npm
 import randomColor from 'randomcolor';
 
+// create a random color
 const color = '#' + Math.floor(Math.random() * 16777216).toString(16);
 
+// create the array-construct of block including random color hex code
 const row = [
   `   ###############################
    ###############################
@@ -18,8 +22,11 @@ const row = [
   `,
 ];
 
+//
 if (process.argv.length < 3) {
   console.log(chalk.hex(color).bold(row));
+
+  //
 } else {
   console.log(
     chalk.hex(
